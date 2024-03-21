a0231acac19f44ea144a5a6ab47046c7fadb59b7
OAguilaLira <omaraguilalira2@gmail.com>
2024-03-21 09:22:45 -0600

    paginaFuncional

--- scripts/index.js
+++ scripts/index.js
@@ -22,34 +22,99 @@ class Repository {
   }
   deleteActivity(id) {
     this.activities = this.activities.filter(
-      (actividad) => actividad.id !== id
+      (actividad) => actividad.id !== Number(id)
     );
+    console.log(this.activities);
   }
 }
 
+// Repositorio de todas las actividades
 const nuevoRepositorio = new Repository();
-console.log(nuevoRepositorio.getAllAtivities());
+// Convertir cada objeto Activity en un elemento HTML
+const actividadObjetoAactividadHTML = (objetoActividad) => {
+  const {id, title, description, imgUrl} = objetoActividad;
+  const tarjeta = document.createElement('div');
+  // const botonEliminar = document.createElement('button');
+  const imagenBotonEliminar = document.createElement('img');
+  imagenBotonEliminar.addEventListener('click', eliminarTarjeta);
 
-nuevoRepositorio.createActivity(
-  "dormir",
-  "Proceso de cerrar los ojos para descanzar",
-  "http://img"
-);
-console.log(nuevoRepositorio.getAllAtivities());
 
-nuevoRepositorio.createActivity(
-  "comer",
-  "Ingerir alimentos para su postenrior digestion",
-  "http://img2"
-);
-console.log(nuevoRepositorio.getAllAtivities());
+  const tarjetaTexto = document.createElement('div');
+  const tarjetaTitulo = document.createElement('h3');
+  const tarjetaDescripcion = document.createElement('p');
+  const tarjetaImagen = document.createElement('img');
+  tarjeta.id = id;
+  imagenBotonEliminar.src = "../assets/img/iconoborrar1.png";
+  imagenBotonEliminar.classList.add('imagenBotonEliminar');
+  imagenBotonEliminar.title = 'Eliminar';
+  imagenBotonEliminar.id = `imagenBotonEliminar-${id}`;
+  tarjetaTexto.classList.add('tarjetaTexto')
+  tarjetaTitulo.innerHTML = title;
+  tarjetaTitulo.classList.add('tarjetaTitulo')
+  tarjetaDescripcion.innerHTML = description;
+  tarjetaDescripcion.classList.add('tarjetaDescripcion')
+  tarjetaImagen.src = imgUrl;
+  tarjetaImagen.alt = `${title}`;
+  tarjetaImagen.classList.add('tarjetaImagen')
+  tarjeta.classList.add('tarjetaActividadFavorita');
+  // botonEliminar.appendChild(imagenBotonEliminar);
+  // botonEliminar.classList.add('botonEliminar')
+  tarjetaTexto.appendChild(tarjetaTitulo);
+  tarjetaTexto.appendChild(tarjetaDescripcion);
+  tarjeta.appendChild(imagenBotonEliminar);
+  tarjeta.appendChild(tarjetaImagen);
+  tarjeta.appendChild(tarjetaTexto);
+  
+  return tarjeta;
+}
 
-nuevoRepositorio.createActivity(
-  "Hacer ejercicio",
-  "Realizar movimientos repetitivos con cargas de peso para tener buena salud",
-  "http://img3"
-);
-console.log(nuevoRepositorio.getAllAtivities());
+// Convertir todas los objetos Activity a elementos HTML
+const actividadesAhtml = () => {
+  const contenedorActividades = document.getElementById('contenedorTarjetasActividades');
+  contenedorActividades.innerHTML = '';
+  const actividades = nuevoRepositorio.getAllAtivities()
+  const actividadesComoHTML = actividades.map((objetoActividad) => {
+    return actividadObjetoAactividadHTML(objetoActividad);
+  })
+  actividadesComoHTML.forEach((actividadHTML) => {
+    contenedorActividades.appendChild(actividadHTML)
+  });
+} 
 
-nuevoRepositorio.deleteActivity(1);
-console.log(nuevoRepositorio.getAllAtivities());
+// Función que se ejecutará al dar click al boton agregar actividad
+const agragarActividades = () => {
+  const inputTitulo = document.getElementById('tituloActividad');
+  const inputDescripcion = document.getElementById('descripcionActividad');
+  const inputImagen = document.getElementById('linkImagen');
+  
+  const titulo = inputTitulo.value;
+  const descripcion = inputDescripcion.value;
+  const imagen = inputImagen.value;
+  
+  // Validar que los campos estén completos 
+  if (!titulo){
+    return alert('Por favor complete el campo: Nombre de la actividad');
+  }
+  else if(!descripcion){
+    return alert('Por favor complete el campo: Descripción de la actividad')
+  }
+  else if(!imagen) {
+    return alert('Por favor complete el campo: Link de la imagen')
+  }
+  nuevoRepositorio.createActivity(titulo, descripcion, imagen);
+  actividadesAhtml();
+  inputTitulo.value = '';
+  inputDescripcion.value = '';
+  inputImagen.value = '';
+}
+
+// Agregar el event listener al botón agregar actividad
+const botonAgregarActividad = document.getElementById('BotonAgregarActividad');
+botonAgregarActividad.addEventListener('click', agragarActividades);
+
+// Función que se encargará de eliminar una tarjeta
+function eliminarTarjeta(evento) {
+  const id = evento.target.id.substr(-1);
+  nuevoRepositorio.deleteActivity(id);
+  actividadesAhtml();
+}
